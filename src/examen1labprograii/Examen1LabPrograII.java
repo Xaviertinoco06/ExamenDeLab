38b953a58f97a31f58332e7bad6437081cda6426
Xaviertinoco06 <xaviertinoco006@gmail.com>
2022-02-18 13:26:24 -0600

    commit 1

---
+++ src/examen1labprograii/Examen1LabPrograII.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen1labprograii;
+
+/**
+ *
+ * @author xavie
+ */
+public class Examen1LabPrograII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
